97c9e478d954e99904d8ab29c74eba250fd6ac22
Omar Ismail <oismail@digitalocean.com>
2018-11-16 14:33:58 -0500

    change url to have :cluster_id

--- lib/droplet_kit/resources/kubernetes_resource.rb
+++ lib/droplet_kit/resources/kubernetes_resource.rb
@@ -7,40 +7,40 @@ module DropletKit
         handler(200) { |response| KubernetesMapping.extract_collection(response.body, :read) }
       end
 
-      action :find, 'GET /v2/kubernetes/clusters/:id' do
+      action :find, 'GET /v2/kubernetes/clusters/:cluster_id' do
       end
 
       action :create, 'POST /v2/kubernetes/clusters' do
       end
 
-      action :config, 'GET /v2/kubernetes/clusters/:id/kubeconfig' do
+      action :config, 'GET /v2/kubernetes/clusters/:cluster_id/kubeconfig' do
       end
 
-      action :update, 'PUT /v2/kubernetes/clusters/:id' do
+      action :update, 'PUT /v2/kubernetes/clusters/:cluster_id' do
       end
 
-      action :upgrade, 'GET /v2/kubernetes/clusters/:id/upgrade' do
+      action :upgrade, 'GET /v2/kubernetes/clusters/:cluster_id/upgrade' do
       end
 
-      action :delete, 'DELETE /v2/kubernetes/clusters/:id' do
+      action :delete, 'DELETE /v2/kubernetes/clusters/:cluster_id' do
       end
 
-      action :all_node_pools, 'GET /v2/kubernetes/clusters/:id/node_pools' do
+      action :all_node_pools, 'GET /v2/kubernetes/clusters/:cluster_id/node_pools' do
       end
 
-      action :find_node_pool, 'GET /v2/kubernetes/clusters/:id/node_pools/:pool_id' do
+      action :find_node_pool, 'GET /v2/kubernetes/clusters/:cluster_id/node_pools/:pool_id' do
       end
 
-      action :create_node_pool, 'POST /v2/kubernetes/clusters/:id/node_pools' do
+      action :create_node_pool, 'POST /v2/kubernetes/clusters/:cluster_id/node_pools' do
       end
 
-      action :update_node_pool, 'PUT /v2/kubernetes/clusters/:id/node_pools/:pool_id' do
+      action :update_node_pool, 'PUT /v2/kubernetes/clusters/:cluster_id/node_pools/:pool_id' do
       end
 
-      action :delete_node_pool, 'DELETE /v2/kubernetes/clusters/:id/node_pools/:pool_id' do
+      action :delete_node_pool, 'DELETE /v2/kubernetes/clusters/:cluster_id/node_pools/:pool_id' do
       end
 
-      action :recycle_node_pool, 'POST /v2/kubernetes/clusters/:id/node_pools/:pool_id/recycle' do
+      action :recycle_node_pool, 'POST /v2/kubernetes/clusters/:cluster_id/node_pools/:pool_id/recycle' do
       end
 
       action :get_options, 'GET /v2/kubernetes/options' do
