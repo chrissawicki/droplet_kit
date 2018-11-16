f43316de8dd200429f78edc3235bfd61d6f29352
Omar Ismail <oismail@digitalocean.com>
2018-11-16 14:31:45 -0500

    Added endpoints for Kubernetes

--- lib/droplet_kit/resources/kubernetes_resource.rb
+++ lib/droplet_kit/resources/kubernetes_resource.rb
@@ -6,11 +6,46 @@ module DropletKit
       action :all, 'GET /v2/kubernetes/clusters' do
         handler(200) { |response| KubernetesMapping.extract_collection(response.body, :read) }
       end
+
+      action :find, 'GET /v2/kubernetes/clusters/:id' do
+      end
+
+      action :create, 'POST /v2/kubernetes/clusters' do
+      end
+
+      action :config, 'GET /v2/kubernetes/clusters/:id/kubeconfig' do
+      end
+
+      action :update, 'PUT /v2/kubernetes/clusters/:id' do
+      end
+
+      action :upgrade, 'GET /v2/kubernetes/clusters/:id/upgrade' do
+      end
+
+      action :delete, 'DELETE /v2/kubernetes/clusters/:id' do
+      end
+
+      action :all_node_pools, 'GET /v2/kubernetes/clusters/:id/node_pools' do
+      end
+
+      action :find_node_pool, 'GET /v2/kubernetes/clusters/:id/node_pools/:pool_id' do
+      end
+
+      action :create_node_pool, 'POST /v2/kubernetes/clusters/:id/node_pools' do
+      end
+
+      action :update_node_pool, 'PUT /v2/kubernetes/clusters/:id/node_pools/:pool_id' do
+      end
+
+      action :delete_node_pool, 'DELETE /v2/kubernetes/clusters/:id/node_pools/:pool_id' do
+      end
+
+      action :recycle_node_pool, 'POST /v2/kubernetes/clusters/:id/node_pools/:pool_id/recycle' do
+      end
+
+      action :get_options, 'GET /v2/kubernetes/options' do
+      end
     end
 
-    #
-    # def all(*args)
-    #   PaginatedResource.new(action(:all), self, *args)
-    # end
   end
 end
